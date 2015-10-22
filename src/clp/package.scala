77dd42a7d5d135ed52ec6cdd9af493088f3df576
N. Justus <nicola.justus@mni.thm.de>
2015-10-22 17:01:14 +0200

    Changed project-structure

--- src/clp/package.scala
+++
@@ -1,9 +0,0 @@
-import clp.config._
-
-package object clp {
-  import types._
-
-  implicit def tupleToConfig[A](tup: (String, ParseTy)): Config = tup match {
-    case (name:String, ty:ParseTy) => config.DefaultConfig(List(name), ty)
-  }
-}
